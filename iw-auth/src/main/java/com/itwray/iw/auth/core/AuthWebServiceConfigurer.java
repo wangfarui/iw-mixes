b221a0e99993b6cdca2c1c0ee93ffa454811b13c
Wray <wray20156294@gmail.com>
2024-03-06 13:56:06 +0800

    新增异常处理拦截器

--- iw-auth/src/main/java/com/itwray/iw/auth/core/AuthWebServiceConfigurer.java
+++ iw-auth/src/main/java/com/itwray/iw/auth/core/AuthWebServiceConfigurer.java
@@ -1,7 +1,20 @@
-package com.itwray.iw.auth.core;/**
- * Description
+package com.itwray.iw.auth.core;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * 授权Web服务配置器
  *
  * @author wray
  * @since 2024/3/5
- */public class AuthWebServiceConfigurer {
+ */
+@Configuration
+public class AuthWebServiceConfigurer implements WebMvcConfigurer {
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new ExceptionHandlerInterceptor());
+    }
 }
--- iw-auth/src/main/java/com/itwray/iw/auth/core/ExceptionHandlerInterceptor.java
+++ iw-auth/src/main/java/com/itwray/iw/auth/core/ExceptionHandlerInterceptor.java
@@ -1,7 +1,55 @@
-package com.itwray.iw.auth.core;/**
- * Description
+package com.itwray.iw.auth.core;
+
+import cn.hutool.json.JSONUtil;
+import com.itwray.iw.auth.model.ApiCode;
+import com.itwray.iw.auth.model.GeneralApiCode;
+import com.itwray.iw.auth.model.GeneralResponse;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.boot.context.properties.bind.validation.BindValidationException;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.lang.Nullable;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+/**
+ * 异常处理拦截器
  *
  * @author wray
  * @since 2024/3/5
- */public class ExceptionHandlerInterceptor {
+ */
+public class ExceptionHandlerInterceptor implements HandlerInterceptor {
+
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler,
+                                @Nullable Exception ex) throws Exception {
+        if (ex == null) {
+            return;
+        }
+
+        GeneralApiCode serverError = GeneralApiCode.SERVER_ERROR;
+        response.setStatus(serverError.getCode());
+        response.setHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON.toString());
+        response.setCharacterEncoding("utf-8");
+
+        GeneralResponse<Object> generalResponse = new GeneralResponse<>();
+        generalResponse.setCode(serverError.getCode());
+        if (supportThrowException(ex)) {
+            generalResponse.setMessage(ex.getMessage());
+        } else {
+            generalResponse.setMessage(serverError.getMessage());
+        }
+
+        response.getWriter().write(JSONUtil.toJsonStr(generalResponse));
+    }
+
+    private boolean supportThrowException(Exception e) {
+        if (e instanceof ApiCode) {
+            return true;
+        }
+        if (e instanceof BindValidationException) {
+            return true;
+        }
+        return false;
+    }
 }
