7fa197685c4b6290ae2556efd589175d895dd60e
Wray <wray20156294@gmail.com>
2024-03-12 17:02:03 +0800

    新增笔记服务模块

---
+++ iw-note/src/main/java/com/itwray/iw/note/IwNoteApplication.java
@@ -0,0 +1,18 @@
+package com.itwray.iw.note;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * IW笔记服务应用程序
+ *
+ * @author wray
+ * @since 2024/3/12
+ */
+@SpringBootApplication
+public class IwNoteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IwNoteApplication.class);
+    }
+}
