a6550e876dbc3b17218f97c41a754f8a04dba3d8
Wray <wray20156294@gmail.com>
2024-03-13 10:36:02 +0800

    授权服务新增用户服务测试类

---
+++ iw-auth/src/test/java/com/itwray/iw/auth/test/AuthUserServiceTest.java
@@ -0,0 +1,40 @@
+package com.itwray.iw.auth.test;
+
+import com.itwray.iw.auth.model.dto.LoginPasswordDto;
+import com.itwray.iw.auth.model.dto.RegisterFormDto;
+import com.itwray.iw.auth.model.vo.UserInfoVo;
+import com.itwray.iw.auth.service.AuthUserService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+/**
+ * 授权服务测试类
+ *
+ * @author wray
+ * @since 2024/3/12
+ */
+@SpringBootTest
+public class AuthUserServiceTest {
+
+    @Autowired
+    private AuthUserService authUserService;
+
+    @Test
+    public void testRegisterByForm() {
+        RegisterFormDto dto = new RegisterFormDto();
+        dto.setUsername("wray");
+        dto.setPassword("123456");
+        authUserService.registerByForm(dto);
+        System.out.println("注册成功");
+    }
+
+    @Test
+    public void testLoginByPassword() {
+        LoginPasswordDto dto = new LoginPasswordDto();
+        dto.setUsername("wray");
+        dto.setPassword("123456");
+        UserInfoVo vo = authUserService.loginByPassword(dto);
+        System.out.println(vo);
+    }
+}
