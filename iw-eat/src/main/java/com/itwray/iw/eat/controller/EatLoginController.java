bdf90cca41631fc83a3053f56fab2351c6708e30
Wray <wray20156294@gmail.com>
2024-05-13 17:50:05 +0800

    feat:引入nacos config服务

--- iw-eat/src/main/java/com/itwray/iw/eat/controller/EatLoginController.java
+++ iw-eat/src/main/java/com/itwray/iw/eat/controller/EatLoginController.java
@@ -1,16 +1,16 @@
 package com.itwray.iw.eat.controller;
 
+import cn.dev33.satoken.annotation.SaIgnore;
 import com.itwray.iw.common.GeneralResponse;
 import com.itwray.iw.eat.model.dto.UserLoginDto;
 import com.itwray.iw.eat.model.vo.UserLoginVo;
 import com.itwray.iw.eat.service.EatUserService;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.annotation.Resource;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * 登录接口
@@ -22,13 +22,23 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/login")
 @Validated
 @Tag(name = "登录接口")
+@RefreshScope
 public class EatLoginController {
 
     @Resource
     private EatUserService eatUserService;
 
+    @Value(value = "${login.user-name:}")
+    private String userName;
+
     @PostMapping("/doLogin")
     public GeneralResponse<UserLoginVo> doLogin(@RequestBody UserLoginDto dto) {
         return GeneralResponse.success(eatUserService.doLogin(dto));
+    }
+
+    @GetMapping("/get")
+    @SaIgnore
+    public String get() {
+        return this.userName;
     }
 }
