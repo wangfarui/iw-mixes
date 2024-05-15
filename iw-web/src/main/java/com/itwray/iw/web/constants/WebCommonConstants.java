b26beef9658799bdeac94af4637f330e92ebdcae
Wray <wray20156294@gmail.com>
2024-05-15 18:10:54 +0800

    feat:web服务增加常量累

---
+++ iw-web/src/main/java/com/itwray/iw/web/constants/WebCommonConstants.java
@@ -0,0 +1,15 @@
+package com.itwray.iw.web.constants;
+
+/**
+ * web公共常量
+ *
+ * @author wray
+ * @since 2024/5/15
+ */
+public abstract class WebCommonConstants {
+
+    /**
+     * 数据库 limit 1 固定语法
+     */
+    public static final String LIMIT_ONE = "limit 1";
+}
