b09d275ed0caa49f71e0c3d49dac205b122d8610
Wray <wray20156294@gmail.com>
2024-08-23 18:01:05 +0800

    refactor:移除sa-token模块

--- iw-eat/src/main/java/com/itwray/iw/eat/controller/EatLoginController.java
+++ iw-eat/src/main/java/com/itwray/iw/eat/controller/EatLoginController.java
@@ -1,12 +1,9 @@
 package com.itwray.iw.eat.controller;
 
-import cn.dev33.satoken.annotation.SaIgnore;
 import com.itwray.iw.common.GeneralResponse;
 import com.itwray.iw.eat.model.dto.UserLoginDto;
 import com.itwray.iw.eat.model.vo.UserLoginVo;
-import com.itwray.iw.eat.service.EatUserService;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import jakarta.annotation.Resource;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.validation.annotation.Validated;
@@ -25,19 +22,16 @@ import org.springframework.web.bind.annotation.*;
 @RefreshScope
 public class EatLoginController {
 
-    @Resource
-    private EatUserService eatUserService;
 
     @Value(value = "${login.user-name:}")
     private String userName;
 
     @PostMapping("/doLogin")
     public GeneralResponse<UserLoginVo> doLogin(@RequestBody UserLoginDto dto) {
-        return GeneralResponse.success(eatUserService.doLogin(dto));
+        return GeneralResponse.success();
     }
 
     @GetMapping("/get")
-    @SaIgnore
     public String get() {
         return this.userName;
     }
--- iw-eat/src/main/java/com/itwray/iw/eat/controller/EatUserController.java
+++ iw-eat/src/main/java/com/itwray/iw/eat/controller/EatUserController.java
@@ -1,9 +1,7 @@
 package com.itwray.iw.eat.controller;
 
-import com.itwray.iw.eat.service.EatUserService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import jakarta.annotation.Resource;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -24,13 +22,9 @@ import java.util.Map;
 @Tag(name = "用户接口")
 public class EatUserController {
 
-    @Resource
-    private EatUserService eatUserService;
-
     @PostMapping("/editAvatar")
     @Operation(summary = "编辑头像")
     public void editAvatar(@RequestBody Map<String, Object> body) {
-        String avatar = (String) body.get("avatar");
-        eatUserService.editAvatar(avatar);
+
     }
 }
