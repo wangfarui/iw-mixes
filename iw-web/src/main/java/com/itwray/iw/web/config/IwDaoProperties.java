8209f5e170df60ef6c3bcf81ef0351e1222713c6
Wray <wray20156294@gmail.com>
2024-09-09 16:42:07 +0800

    perf:优化数据权限逻辑

--- iw-web/src/main/java/com/itwray/iw/web/config/IwDaoProperties.java
+++ iw-web/src/main/java/com/itwray/iw/web/config/IwDaoProperties.java
@@ -1,10 +1,12 @@
 package com.itwray.iw.web.config;
 
+import cn.hutool.core.collection.CollUtil;
 import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.data.annotation.Transient;
 import org.springframework.validation.annotation.Validated;
 
-import java.util.List;
+import java.util.*;
 
 /**
  * IW Dao 属性配置
@@ -18,7 +20,7 @@ import java.util.List;
 public class IwDaoProperties {
 
     /**
-     * 启用分页插件
+     * 启用分页插件(默认true)
      */
     private boolean enablePagination = true;
 
@@ -27,18 +29,40 @@ public class IwDaoProperties {
      */
     private DataPermission dataPermission = new DataPermission();
 
+    /**
+     * 数据表的数据权限配置
+     */
     @Data
     public static class DataPermission {
 
         /**
-         * 启用数据权限
+         * 启用数据权限(默认true)
+         * <p>只有在{@code enabled=true}时，enableTableNames和disableTableNames 才会生效，如果两个列表数据都为空，则全部数据表启用数据权限。</p>
          */
         private boolean enabled = true;
 
         /**
-         * 禁用数据权限的表名
+         * 启用数据权限的表名
+         * <p>用于只希望启用个别数据表的数据权限。</p>
          */
-        private List<String> disableTableNames;
+        private Set<String> enableTableNames;
+
+        /**
+         * 禁用数据权限的表名
+         * <p>用于只希望禁用个别数据表的数据权限。</p>
+         * <p>更希望web服务针对禁用表做控制。</p>
+         * <p>禁用数据表列表的优先级大于启用数据表列表。换言之，只要disableTableNames配置了表a，则表a禁用，如果没有配置，而enableTableNames不为空且未配置，也表示禁用。</p>
+         */
+        private Set<String> disableTableNames;
+
+        /**
+         * 被禁用数据权限的数据表状态缓存
+         * true -> 禁用
+         * false -> 启用
+         * null -> 未查询过
+         */
+        @Transient
+        private Map<String, Boolean> disableTableStatusCache = new HashMap<>();
 
         /**
          * 数据表的数据权限是否被禁用
@@ -47,7 +71,12 @@ public class IwDaoProperties {
          * @return true -> 被禁用
          */
         public boolean disableTable(String tableName) {
-            return disableTableNames != null && disableTableNames.contains(tableName);
+            return disableTableStatusCache.computeIfAbsent(tableName, key -> {
+                if (CollUtil.isNotEmpty(disableTableNames) && disableTableNames.contains(key)) {
+                    return true;
+                }
+                return CollUtil.isNotEmpty(enableTableNames) && !enableTableNames.contains(key);
+            });
         }
     }
 }
