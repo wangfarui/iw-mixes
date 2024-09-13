7a4404499d22030ee0eff29aaaae3dc8b5d26ca4
Wray <wray20156294@gmail.com>
2024-09-13 11:15:34 +0800

    feat:字典类型枚举新增数据类型

--- iw-web/src/main/java/com/itwray/iw/web/model/enums/DictTypeEnum.java
+++ iw-web/src/main/java/com/itwray/iw/web/model/enums/DictTypeEnum.java
@@ -11,25 +11,38 @@ import lombok.Getter;
 @Getter
 public enum DictTypeEnum {
 
-    /** iw-web **/
-    WEB_TAG(1001, "web模块"),
+    /** iw-web web模块 **/
+    WEB_TAG(1001, "web模块", DataType.ID),
 
-    /** iw-auth **/
-    AUTH_TAG(2001, "授权模块"),
+    /** iw-auth 授权模块 **/
+    AUTH_TAG(2001, "授权模块", DataType.ID),
 
-    /** iw-eat **/
-    EAT_TAG(3001, "餐饮模块"),
+    /** iw-eat 餐饮模块 **/
+    EAT_TAG(3001, "餐饮模块", DataType.ID),
+    EAT_MEAL_TIME(3002, "餐饮-用餐时间", DataType.CODE),
+    EAT_DISHES_TYPE(3003, "餐饮-菜品分类", DataType.CODE),
+    EAT_DISHES_STATUS(3004, "餐饮-菜品状态", DataType.CODE),
 
-    /** iw-bookkeeping **/
-    BOOKKEEPING_TAG(4001, "记账标签"),
+    /** iw-bookkeeping 记账模块 **/
+    BOOKKEEPING_RECORD_TAG(4001, "记账-记录标签", DataType.ID),
+    BOOKKEEPING_RECORD_TYPE(4002, "记账-记录分类", DataType.CODE),
+    BOOKKEEPING_RECORD_CATEGORY(4003, "记账-记录类型", DataType.CODE),
     ;
 
     private final Integer code;
 
     private final String name;
 
-    DictTypeEnum(Integer code, String name) {
+    private final DataType dataType;
+
+    DictTypeEnum(Integer code, String name, DataType dataType) {
         this.code = code;
         this.name = name;
+        this.dataType = dataType;
+    }
+
+    public enum DataType {
+        ID,
+        CODE
     }
 }
