251b85846d66befc07bda4a91f47042aff752161
Wray <wray20156294@gmail.com>
2024-10-15 20:24:27 +0800

    update DateUtils.java

--- iw-common/src/main/java/com/itwray/iw/common/utils/DateUtils.java
+++ iw-common/src/main/java/com/itwray/iw/common/utils/DateUtils.java
@@ -27,18 +27,29 @@ public abstract class DateUtils {
      * 返回当前月的开始日期
      */
     public static LocalDate startDateOfNowMonth() {
-        return LocalDate.now().withDayOfMonth(1);
+        return startDateOfMonth(LocalDate.now());
+    }
+
+    /**
+     * 返回指定月的开始日期
+     */
+    public static LocalDate startDateOfMonth(LocalDate date) {
+        return date.withDayOfMonth(1);
     }
 
     /**
      * 返回当前月的结束日期
      */
     public static LocalDate endDateOfNowMonth() {
-        // 获取当前日期
-        LocalDate today = LocalDate.now();
+        return endDateOfMonth(LocalDate.now());
+    }
 
+    /**
+     * 返回指定月的结束日期
+     */
+    public static LocalDate endDateOfMonth(LocalDate date) {
         // 使用 YearMonth 获取当前年的当前月
-        YearMonth yearMonth = YearMonth.of(today.getYear(), today.getMonth());
+        YearMonth yearMonth = YearMonth.of(date.getYear(), date.getMonth());
 
         // 获取当前月的最后一天
         return yearMonth.atEndOfMonth();
@@ -48,11 +59,15 @@ public abstract class DateUtils {
      * 返回当前月的开始时间
      */
     public static LocalDateTime startTimeOfNowMonth() {
-        // 获取当前日期的 LocalDate
-        LocalDate currentDate = LocalDate.now();
+        return startTimeOfMonth(LocalDate.now());
+    }
 
+    /**
+     * 返回指定月的开始时间
+     */
+    public static LocalDateTime startTimeOfMonth(LocalDate date) {
         // 获取当前月的第一天
-        LocalDate firstDayOfMonth = currentDate.withDayOfMonth(1);
+        LocalDate firstDayOfMonth = date.withDayOfMonth(1);
 
         // 将时间设置为 00:00:00
         return LocalDateTime.of(firstDayOfMonth, LocalTime.MIDNIGHT);
@@ -62,11 +77,15 @@ public abstract class DateUtils {
      * 返回当前月的结束时间
      */
     public static LocalDateTime endTimeOfNowMonth() {
-        // 获取当前日期
-        LocalDate currentDate = LocalDate.now();
+        return endTimeOfMonth(LocalDate.now());
+    }
 
+    /**
+     * 返回指定月的结束时间
+     */
+    public static LocalDateTime endTimeOfMonth(LocalDate date) {
         // 使用 YearMonth 获取当前月最后一天
-        YearMonth yearMonth = YearMonth.of(currentDate.getYear(), currentDate.getMonth());
+        YearMonth yearMonth = YearMonth.of(date.getYear(), date.getMonth());
         LocalDate lastDayOfMonth = yearMonth.atEndOfMonth();
 
         // 将时间设置为 23:59:59
