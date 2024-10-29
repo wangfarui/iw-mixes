bab6612ccb7a4ed0b8c0e2b847ca5190f5138415
Wray <wray20156294@gmail.com>
2024-10-29 21:47:02 +0800

    feat:增加日志链路追踪

--- iw-web/src/main/java/com/itwray/iw/web/core/mybatis/MyBatisApplicationRunListener.java
+++ iw-web/src/main/java/com/itwray/iw/web/core/mybatis/MyBatisApplicationRunListener.java
@@ -1,12 +1,12 @@
 package com.itwray.iw.web.core.mybatis;
 
-import org.apache.ibatis.logging.stdout.StdOutImpl;
+import org.apache.ibatis.logging.slf4j.Slf4jImpl;
 import org.springframework.boot.ConfigurableBootstrapContext;
 import org.springframework.boot.SpringApplicationRunListener;
 import org.springframework.core.env.ConfigurableEnvironment;
 
 /**
- * MyBatis应用启动监听器
+ * MyBatis 应用启动监听器
  *
  * @author wray
  * @since 2024/10/15
@@ -15,7 +15,7 @@ public class MyBatisApplicationRunListener implements SpringApplicationRunListen
 
     @Override
     public void environmentPrepared(ConfigurableBootstrapContext bootstrapContext, ConfigurableEnvironment environment) {
-        environment.getSystemProperties().put("mybatis-plus.configuration.log-impl", StdOutImpl.class);
+        environment.getSystemProperties().put("mybatis-plus.configuration.log-impl", Slf4jImpl.class);
         environment.getSystemProperties().put("mybatis-plus.type-handlers-package", "com.itwray.iw.web.mybatis.type.handlers");
     }
 }
