6d49e62417fbf225f71899f97de5e8271dc36b53
bubu <farui.wang@outlook.com>
2025-03-12 18:09:07 +0800

    feat:web请求增加出入参日志打印

--- iw-web/src/main/java/com/itwray/iw/web/core/webmvc/IwWebMvcConfiguration.java
+++ iw-web/src/main/java/com/itwray/iw/web/core/webmvc/IwWebMvcConfiguration.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.databind.SerializationFeature;
 import org.springframework.boot.autoconfigure.jackson.Jackson2ObjectMapperBuilderCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -29,5 +30,14 @@ public class IwWebMvcConfiguration implements WebMvcConfigurer {
     public void addInterceptors(InterceptorRegistry registry) {
         // 注册拦截器，并指定拦截的路径
         registry.addInterceptor(new WebHandlerInterceptor()).addPathPatterns("/**");  // 拦截所有路径
+    }
+
+    /**
+     * web请求日志过滤器 配置类
+     */
+    @Configuration
+    @Import({RequestLoggingFilter.class, ResponseLoggingFilter.class})
+    public static class WebLoggingFilterConfiguration {
+
     }
 }
