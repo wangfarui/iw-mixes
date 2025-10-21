a458e87ce92ed7db4110df77825902df7052ff57
Wray <wray20156294@gmail.com>
2025-10-21 14:02:14 +0800

    fix:修复记账分页空指针异常问题

--- iw-packaging-parent/iw-bookkeeping/src/main/java/com/itwray/iw/bookkeeping/service/impl/BookkeepingRecordsServiceImpl.java
+++ iw-packaging-parent/iw-bookkeeping/src/main/java/com/itwray/iw/bookkeeping/service/impl/BookkeepingRecordsServiceImpl.java
@@ -293,10 +293,14 @@ public class BookkeepingRecordsServiceImpl extends WebServiceImpl<BookkeepingRec
 
     private void processBookkeepingRecordPageDto(BookkeepingRecordPageDto dto) {
         if (dto.getRecordStartDate() == null) {
-            dto.setRecordStartDate(DateUtils.startDateOfMonth(dto.getRecordEndDate()));
+            if (dto.getRecordEndDate() != null) {
+                dto.setRecordStartDate(DateUtils.startDateOfMonth(dto.getRecordEndDate()));
+            }
         }
         if (dto.getRecordEndDate() == null) {
-            dto.setRecordEndDate(DateUtils.endDateOfMonth(dto.getRecordStartDate()));
+            if (dto.getRecordStartDate() != null) {
+                dto.setRecordEndDate(DateUtils.endDateOfMonth(dto.getRecordStartDate()));
+            }
         }
         if (CollUtil.isNotEmpty(dto.getTagIdList())) {
             dto.setTagBusinessType(DictBusinessTypeEnum.BOOKKEEPING_RECORD_TAG.getCode());
